d43a10c09e46577089cd94cfc049ffbf84c8463b
Scott O'Neil <scottieoneil@gmail.com>
2020-12-31 16:26:01 -0800

    finish logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-$(document).ready(function() {
-    document.getElementById("submit").onsubmit = function() {
+const beep = "Beep!"
+const boop = "BOOP!"
+const neighb = "Won't you be my neighbor?"
 
-    }
-})
+let robo = function(end) {
+    let array = [];
+    for (i = 0; i <= end; i++) {
+        array.push(i.toString());
+    };
+
+    let robog = array.map(function(arrays) {
+        if (arrays.includes("3")) {
+            return arrays = neighb;
+        } else if (arrays.includes("2")) {
+            return arrays = boop;
+        } else if (arrays.includes("1")) {
+            return arrays = beep;
+        } else {
+            return arrays;
+        };
+    });
+    return robog;
+};
+
+
+$(document).ready(function() {
+    $("form#submit").submit(function(event) {
+        event.preventDefault();
+        let roboger = parseInt($("input#robo").val());
+        let results = robo(roboger);
+        $("#answer").text(results);
+        $("#answer").toggle();
+    });
+});
